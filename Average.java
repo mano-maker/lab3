5dd6d9b2e172640692dbda8701525b31f2f07705
mano-maker <130724751+mano-maker@users.noreply.github.com>
2023-11-22 18:15:23 +0500

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+class Average{
+public static void main(String [] args){
+Scanner s=new Scanner(System.in);
+System.out.println("Enter your num in 3 sub");
+double sub1=s.nextDouble();
+double sub2=s.nextDouble();
+double sub3=s.nextDouble();
+double ave=(sub1+sub2+sub3)/3;
+if(ave>50){
+System.out.println("PASS");
+
+}
+else{
+System.out.println("FAIL");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+}
